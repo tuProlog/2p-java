c931171a62a3864d35776f5f3b3ac2912c5dd8b1
Giovanni <giovanni.ciatto@unibo.it>
2019-04-04 12:32:34 +0200

    concordion tests integrated, some are still failing

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -11,7 +11,18 @@ repositories {
 dependencies {
     implementation("com.google.code.gson", "gson", "2.8.5")
     implementation("org.apache.commons", "commons-lang3", "3.8.1")
-    testCompile("junit", "junit", "4.12")
+    testImplementation("junit", "junit", "4.12")
+    testImplementation("org.concordion", "concordion", "2.+")
+    testImplementation("org.concordion", "concordion-embed-extension", "1.2.0")
+}
+
+tasks.getByName<Test>("test") {
+    // display test output on console
+    testLogging.showStandardStreams = true
+    // write output to build/reports/spec
+    systemProperties["concordion.output.dir"] = "${reporting.baseDir}/spec"
+    // force tests to run even if code hasn't changed
+    outputs.upToDateWhen { false }   
 }
 
 configure<JavaPluginConvention> {
