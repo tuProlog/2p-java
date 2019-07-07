51254ab798e0e86ea411431655e6af4cd06120c8
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2019-07-07 17:55:41 +0200

    fix array get/set access

--- core/src/main/resources/alice/tuprolog/lib/OOLibrary.pl
+++ core/src/main/resources/alice/tuprolog/lib/OOLibrary.pl
@@ -15,14 +15,32 @@ Obj <- What returns Res :-
     java_call(Obj, What, Res).
 
 array_set(Array, Index, Object) :-
-    class('java.lang.reflect.Array') <- set(Array as 'java.lang.Object', Index, Object as 'java.lang.Object'), !.
+    java_catch(
+        (class('java.lang.reflect.Array') <- set(Array as 'java.lang.Object', Index, Object as 'java.lang.Object'), !),
+        [
+            (E, java_array_set_primitive(Array, Index, Object))
+            %(E, fail)
+        ],
+        true
+    ).
+/*
 array_set(Array, Index, Object) :-
     java_array_set_primitive(Array, Index, Object).
+*/
 
 array_get(Array, Index, Object) :-
-    class('java.lang.reflect.Array') <- get(Array as 'java.lang.Object', Index) returns Object, !.
+    java_catch(
+        (class('java.lang.reflect.Array') <- get(Array as 'java.lang.Object', Index) returns Object, !),
+        [
+            (E, java_array_get_primitive(Array, Index, Object))
+            %(E, fail)
+        ],
+        true
+    ).
+/*
 array_get(Array, Index, Object) :-
     java_array_get_primitive(Array, Index, Object).
+*/
 
 array_length(Array, Length) :-
     class('java.lang.reflect.Array') <- getLength(Array as 'java.lang.Object') returns Length.
@@ -33,14 +51,32 @@ java_object_bt(ClassName, Args, Id) :-
     destroy_object(Id).
 
 java_array_set(Array, Index, Object) :-
-    class('java.lang.reflect.Array') <- set(Array as 'java.lang.Object', Index, Object as 'java.lang.Object'), !.
+    java_catch(
+        (class('java.lang.reflect.Array') <- set(Array as 'java.lang.Object', Index, Object as 'java.lang.Object'), !),
+        [
+            (E, java_array_set_primitive(Array, Index, Object))
+            %(E, fail)
+        ],
+        true
+    ).
+/*
 java_array_set(Array, Index, Object) :-
     java_array_set_primitive(Array, Index, Object).
+*/
 
 java_array_get(Array, Index, Object) :-
-    class('java.lang.reflect.Array') <- get(Array as 'java.lang.Object', Index) returns Object, !.
+    java_catch(
+        (class('java.lang.reflect.Array') <- get(Array as 'java.lang.Object', Index) returns Object, !),
+        [
+            (E, java_array_get_primitive(Array, Index, Object))
+            %(E, fail)
+        ],
+        true
+    ).
+/*
 java_array_get(Array, Index, Object) :-
     java_array_get_primitive(Array, Index, Object).
+*/
 
 java_array_length(Array, Length) :-
     class('java.lang.reflect.Array') <- getLength(Array as 'java.lang.Object') returns Length.
